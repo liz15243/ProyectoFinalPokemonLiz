0a733d68f8fc999b751791cd762a025f297a784a
liz15243 <lizethespi11@gmail.com>
2022-04-06 11:41:06 -0500

    Clases realizadas 06/04

--- src/Pocion.java
+++ src/Pocion.java
@@ -4,6 +4,27 @@ public class Pocion extends Objeto{
     private String efecto;
     private int duracion;
 
+    public String getEfecto() {
+        return efecto;
+    }
+
+    public void setEfecto(String efecto) {
+        this.efecto = efecto;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public Pocion(double costo, int cantidad, String nombre, String tipo, String efecto, int duracion) {
+        super(costo, cantidad, nombre, tipo);
+        this.efecto = efecto;
+        this.duracion = duracion;
+    }
     // Usar
        // Si es curacion
          // Aumentar 20 hp
